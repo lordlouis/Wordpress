0307c444c7fd3d7ec9cf103ffba863ad717fae25
lordlouis <lake.lordluis@gmail.com>
2016-06-30 09:35:45 -0600

    first commit

---
+++ jquery-dopbsp-color-seat.js
@@ -0,0 +1,19 @@
+/**
+ * Pinpoint Booking System WordPress Plugin
+ * Script para cambiar el color del calendario a los dias de eventos 
+ * que solo tengan 'x' asiento disponibles
+ * 
+ * TODO cambiar DOMSubtreeModified propertychange a Mutation Observers
+ * 
+ * */
+(function($) {
+	var color = "#fdaa29";
+	var seats = 1;
+	$(document).on("DOMSubtreeModified propertychange", ".DOPBSPCalendar-day.dopbsp-available",  function(){
+		var avaiables = $(".dopbsp-body .dopbsp-available:last", this).text().split(' ')[0];
+		if ($.isNumeric(avaiables) && avaiables == seats){
+			// Se quita la clase dopbsp-available, para que tome el nuevo color que se le asigna
+			$(".dopbsp-head .dopbsp-co, .dopbsp-head .dopbsp-ci", this).removeClass("dopbsp-available").css("background-color", color);
+		}
+	});
+})( jQuery );
